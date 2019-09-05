b50f81185dc07d02b490c31bc7f4d5d16caa280d
Ania <zegota.anka@gmail.com>
2019-09-05 22:34:28 +0200

    Added Calculator class

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.calculator;
+
+public class Calculator {
+
+    public static int addAToB(int a, int b) {
+        return a+b;
+    }
+
+    public static int substractAFromB(int a, int b) {
+        return a-b;
+    }
+
+    public static void main(String[] args) {
+        addAToB(5, 3);
+        System.out.println(addAToB(5, 3));
+  }
+}
